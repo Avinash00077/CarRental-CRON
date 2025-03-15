6c577fe65b732d30bdf6f5e06450339413dae1dd
Avinash Reddy <avinashreddytummuri77@gmail.com>
2025-03-15 12:39:14 +0530

    removed

--- server.js
+++ server.js
@@ -8,8 +8,4 @@ cron.schedule('0 0 * * *', async () => {
     logger.info("Expired bookings job executed successfully");
 });
 
-cron.schedule('* * * * * *', () => {
-    logger.info("Expired bookings job executed successfully (Every Second)");
-});
-
 logger.info('cronjob pod is started')
